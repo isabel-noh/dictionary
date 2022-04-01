25ae435b30314d963e0991e16e1e931f7937e21f
isabel-noh <nohhyunjeong93@gmail.com>
2022-04-01 13:28:06 +0900

    Update dictionary.js
    update addWord, deleteWord, done/undo Word

--- src/redux/modules/dictionary.js
+++ src/redux/modules/dictionary.js
@@ -6,28 +6,32 @@ const LOAD = "dictionary/LOAD";
 const ADD = "dictionary/ADD";
 const DELETE = "dictionary/DELETE";
 const DONE = "dictionary/DONE";
+const UNDO = "dictionary/UNDO"
 // const EDIT = "dictionary/EDIT";
 
 //Initial State 
 const initialState = {
     list: [
-        {word: 'word', desc: '말, 단어, 이야기', example: 'sentence is made of several words', done: false},],
-        // {word: 'sentence', desc: '문장, 징역을 살다', example: 'the judge sentenced him to a fine of $50 and time served', done: false}
-    
+        {word: 'word', desc: '말, 단어, 이야기', example: 'sentence is made of several words', done: false},
+        {word: 'sentence', desc: '문장, 징역을 살다', example: 'the judge sentenced him to a fine of $50 and time served', done: false}
+    ],
 };
 
 //Action Creaters
 export function loadWord(dictionary_list){
     return{type: LOAD, dictionary_list};
 }
-export function addWord(word, desc, example){
-    return {type: ADD, word, desc, example};
+export function addWord(word){
+    return {type: ADD, word};
 }
-export function deleteWord(index){
-    return {type: DELETE, index};
+export function deleteWord(word_id){
+    return {type: DELETE, word_id};
 }
-export function doneWord(index){
-    return {type:DONE, index};
+export function doneWord(word_id){
+    return {type:DONE, word_id};
+}
+export function undoWord(word_id){
+    return {type:UNDO, word_id};
 }
 // export function editWord(word){
 //     return {type: EDIT, word};
@@ -39,20 +43,42 @@ export const loadDictFB = () => {
         const dictionary_data = await getDocs(collection(db, "dictionary"));
 
         let dict_list = [];
-        // console.log(dictionary_data.data());
         dictionary_data.forEach((x) => {
             dict_list.push({id: x.id, ...x.data()});
-            console.log(dict_list);
         }) 
         dispatch(loadWord(dict_list));
-    }
-}
+    };
+};
 export const addWordFB = (word) => {
-    return async function (dispatch){
+    return async function (dispatch) {
         const docRef = await addDoc(collection(db, "dictionary"), word);
-        console.log((await getDoc(docRef)).data(), docRef.id);
-    }
-}
+        const _word = {id: docRef.id, ...word};
+
+        dispatch(addWord(_word));
+    };
+};
+export const deleteWordFB = (word_id) => {
+    return async function (dispatch, getState) {
+      const docRef = doc(db, "dictionary", word_id);
+      await deleteDoc(docRef);
+      dispatch(deleteWord(word_id));
+    };
+  };
+
+export const doneWordFB = (word_id) => {
+    return async function (dispatch, getState) {
+      const docRef = doc(db, "dictionary", word_id);
+      await updateDoc(docRef, { done: true });
+      dispatch(doneWord(word_id));
+    };  
+};
+export const undoWordFB = (word_id) => {
+    return async function (dispatch, getState) {
+      const docRef = doc(db, "dictionary", word_id);
+      await updateDoc(docRef, { done: false });
+      dispatch(undoWord(word_id));
+    };  
+};
 
 
 //Reducer
@@ -62,26 +88,39 @@ export default function reducer(state = initialState, action = {} ){
             return {list:action.dictionary_list};//
         }
         case "dictionary/ADD": {
-            const new_word = {word: action.word, desc: action.desc, example: action.example, done: false}
-            const new_word_list = [...state.list, new_word];
+            const new_word_list = [
+                ...state.list, {
+                    id: action.word.id, 
+                    done: false, 
+                    word: action.word.word, 
+                    desc: action.word.desc, 
+                    example: action.word.example,
+                }
+            ];
             return {list: new_word_list};
         }
         case "dictionary/DELETE":{
             const new_word_list = state.list.filter((x, idx)=>{
-                return parseInt(action.index) !== idx;
+                return parseInt(action.word_id) !== idx;
             });
             return {list: new_word_list};
         }
         case "dictionary/DONE":{
             const new_word_list = state.list.map((x, idx) =>{
-                if(parseInt(action.index) === idx && x.done === false){
+                if(parseInt(action.word_id) === idx && x.done === false){
                     return {...x, done: true};
-                }else if(parseInt(action.index) === idx && x.done === true){
-                    return {...x, done: false};
-                }                
+                }             
                 else return x;
             });
-            console.log(new_word_list);
+            return {list: new_word_list};
+        }
+        case "dictionary/UNDO":{
+            const new_word_list = state.list.map((x, idx) =>{
+                if(parseInt(action.word_id) === idx && x.done === true){
+                    return {...x, done: false};
+                }
+                else return x;
+            });
             return {list: new_word_list};
         }
         //단어장 수정
